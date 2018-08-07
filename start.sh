49f79d120bed6d0e306db56822541e3f738b78df
Ollin Boer Bohan <madebyollin@gmail.com>
2018-08-07 11:13:55 -0700

    css fixes

--- start.sh
+++ start.sh
@@ -1,3 +1,17 @@
-python -m SimpleHTTPServer 8000 &
-sass --watch sass:css &
-open -a "Google Chrome" http://localhost:8000
+#! /usr/bin/env bash
+SESSION=mri
+PORT=8000
+
+tmux -2 new-session -d -s $SESSION
+tmux new-window -t $SESSION:1 -n $SESSION
+tmux split-window -h -p 20
+tmux select-pane -t 0
+tmux send-keys "python -m http.server $PORT" C-m
+tmux select-pane -t 1
+tmux send-keys "sass --watch scss:css" C-m
+tmux select-window -t $SESSION:1
+tmux select-pane -t 0
+
+open -a "Google Chrome" http://localhost:$PORT
+tmux -2 attach-session -t $SESSION
+
